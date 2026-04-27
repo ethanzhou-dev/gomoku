8e98b222d13651a67ded1c3e9fa1f799bdea06f7
Ethan Zhou <zhouyireitik@gmail.com>
2026-04-27 16:17:00 +0800

    加载显示优化

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 import { Game } from './game.js';
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
     window.game = new Game();
+    document.body.style.visibility = 'visible';
 });
