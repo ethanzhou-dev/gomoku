7122563a69f640c4751787e3a9c1a1143d6dbce0
Ethan Zhou <zhouyireitik@gmail.com>
2026-04-29 17:59:43 +0800

    PWA去除

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,4 @@ import { Game } from './game.js';
 window.addEventListener('load', () => {
     window.game = new Game();
     document.body.classList.add('loaded');
-
-    // Register Service Worker for PWA support
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
-            .then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            })
-            .catch(err => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    }
 });
